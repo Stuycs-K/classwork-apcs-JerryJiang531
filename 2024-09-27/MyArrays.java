9832fc704fb0dd4d4830fd3a2dd33d79825789aa
JerryJiang531 <jerryj36@nycstudents.net>
2024-09-27 10:03:16 -0400

    forgot to save last commit, added aryToString

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,21 +1,22 @@
 public class MyArrays{
   public static void main(String[] args){
-
+      System.out.println(aryToString(new int[]{1,2,3,4}));
   }
   public static String aryToString(int[] nums){
     String result = "[";
     for (int i = 0; i < nums.length; i ++){
       result +=nums[i];
       if (i != nums.length-1){
-        result += nums[i];
+        result += ", ";
       }
     }
     return result += "]";
   }
   public static int[] returnCopy(int[]ary){
-
+    return new int[0];
   }
   public static int[] concatArray(int[]ary1,int[]ary2){
+    return new int[0];
 
   }
 }
