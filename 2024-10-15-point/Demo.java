eeac97373705064728bb48eb9c6459a2a0813960
JerryJiang531 <jerryj36@nycstudents.net>
2024-10-15 09:58:05 -0400

    added more test cases

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -16,9 +16,24 @@ public class Demo{
     Point p1 = new Point(1,1);
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
+    Point p4 = new Point(0,0);
+    Point p5 = new Point(0,4);
+    Point p6 = new Point(4,0);
+    System.out.println( p1);
+    System.out.println( p2);
     System.out.println( p3);
+    System.out.println( p4);
+    System.out.println( p5);
+    System.out.println( p6);
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
+    System.out.println( distance(p4,p5));
+    System.out.println( Point.distance(p4,p5));
+    System.out.println( p4.distanceTo(p5));
+    System.out.println( distance(p4,p3));
+    System.out.println( Point.distance(p4,p3));
+    System.out.println( p4.distanceTo(p3));
+
   }
 }
