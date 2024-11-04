de82b135f56e8be7617968823b743d57c961182a
JerryJiang531 <jerryj36@nycstudents.net>
2024-11-04 10:14:53 -0500

    end of classwork, started on countTrianglesB

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -30,4 +30,30 @@ public class TriangleTester{
     int c = sc.nextInt();
     return (a + b > c) && (a +c > b) && (b+c > a);
   }
+  public static int countTrianglesB(String filename){
+    int sum = 0;
+    try{
+      File file = new File(filename);//1
+      Scanner input = new Scanner(file);
+      String a = "";
+      while (input.hasNextLine()){
+        String temp1 = input.nextLine();
+        String temp2 = input.nextLine();
+        String temp3 = input.nextLine();
+        for (int j = 0; j < 3; j++){
+          a += temp1.nextInt();
+          a += temp2.nextInt();
+          a += temp3.nextInt();
+        }
+        if (isTriangle(a)){
+          sum ++;
+        }
+      }
+
+      input.close();
+    }catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+    }
+    return sum;
+  }
 }
