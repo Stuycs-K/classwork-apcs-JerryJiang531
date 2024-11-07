9445cfefbc8a07b972f9ce1aa7cdd3f7a278d44d
JerryJiang531 <jerryj36@nycstudents.net>
2024-11-07 10:12:04 -0500

    finished step 9

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -6,5 +6,10 @@ public class Driver{
     Fred.speak();
     Bird Birdo = new Bird("Squaaaak", 2, "Birdo", 4.0, "pink");
     Birdo.speak();
+
+    Animal a1 = new Animal("meow", 3, "a1");
+    Bird b1 = new Bird("woof", 2, "b1", 2.0, "light blue");
+    //Bird b2 = new Animal("meow", 3, "b2"); This one don't work
+    Animal a2 = new Bird("woof", 2, "a2", 2.0, "light blue");
   }
 }
