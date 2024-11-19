a5f55c95763d943c420b8f396b43ecdacb85cd10
JerryJiang531 <jerryj36@nycstudents.net>
2024-11-19 10:05:31 -0500

    improved template

--- 2024-11-18-advent2016/day3.java
+++ 2024-11-18-advent2016/day3.java
@@ -1,22 +1,29 @@
 // I forgot I already did 2016 day 3 advent of code, so I'm using it as a template for my other advent of code instead
 // repeatable code for advent of code
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
 public class day3{ // replace with correct day
     public static void main(String[] args){
-        System.out.println(part1("inputDay3.txt")); // replace with correct day
+        String[] data = parseStringArr("inputDay3.txt"); // replace with correct day
+        System.out.println(Arrays.toString(data));
+        System.out.println(solve(data));
     }
-    public static int part1(String filename){ // replace with correct return type
-        try {
-            File file = new File(filename);
-            Scanner input = new Scanner(file);
-            while (input.hasNext()){ // sometimes "hasNextLine()"
-                //code body
-            }
-        }catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        }
-        // add return statement
+    public static String[] parseStringArr(String filename){
+      try {
+          File file = new File(filename);
+          Scanner input = new Scanner(file);
+          String a = "";
+          while (input.hasNextLine()){
+            a += input.nextLine() + "\n";
+          }
+          String[] data = a.split("\n"); // replace with correct split parameter
+          return data;
+      }catch (FileNotFoundException e) {
+          System.out.println("File not found");
+      }
+      return null;
+    }
+    public static int solve(String[] data){ // replace with correct return type
+      return 0;
     }
 }
