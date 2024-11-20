d33927dd2b086916c13c32ce4fa0fef2dc2a2744
JerryJiang531 <jerryj36@nycstudents.net>
2024-11-19 20:21:22 -0500

    day6 part 1 code working

--- 2024-11-18-advent2016/day6.java
+++ 2024-11-18-advent2016/day6.java
@@ -12,12 +12,11 @@ public class day6 { // replace with correct day
             File file = new File(filename);
             Scanner input = new Scanner(file);
             String a = "";
-            int len = 0;
             while (input.hasNextLine()) {
                 a += input.nextLine() + "\n";
             }
             String[] data = a.split("\n");
-            len = data[0].length();// replace with correct split parameter
+            int len = data[0].length();// replace with correct split parameter
             String[] data1 = new String[len];
             for (int i = 0; i < data1.length; i++){
                 data1[i] = "";
@@ -34,6 +33,30 @@ public class day6 { // replace with correct day
         return null;
     }
     public static String part1(String[] data){
-        return "";
+        String res = "";
+        for (int i = 0; i < data.length; i++){
+            res += most(data[i]);
+        }
+        return res;
+    }
+    public static String most(String a){
+        char[] alphabet = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        int[] count = new int[26];
+        for (int i = 0; i < a.length(); i++){
+            for (int j = 0; j < alphabet.length; j++){
+                if (a.charAt(i) == alphabet[j]){
+                    count[j]++;
+                }
+            }
+        }
+        int max = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < count.length; i++){
+            if (count[i] > max){
+                max = count[i];
+                maxIndex = i;
+            }
+        }
+        return "" + alphabet[maxIndex];
     }
 }
