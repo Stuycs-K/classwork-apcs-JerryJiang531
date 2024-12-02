e8067b584a8ada1ed4f97cbdbc8b155b2493cce7
JerryJiang531 <jerryj36@nycstudents.net>
2024-12-02 18:46:14 -0500

    part 1 working

--- advent2024/day2.java
+++ advent2024/day2.java
@@ -1,8 +1,8 @@
 import java.io.*;
 import java.util.*;
-public class day3{ // replace with correct day
+public class day2{ // replace with correct day
     public static void main(String[] args){
-        String[] data = parseStringArr("inputDay3.txt"); // replace with correct day
+        String[] data = parseStringArr("inputDay2.txt"); // replace with correct day
         System.out.println(Arrays.toString(data));
         System.out.println(part1(data));
     }
@@ -22,11 +22,54 @@ public class day3{ // replace with correct day
       return null;
     }
     public static int part1(String[] data){
+        int count  = 0;
       for (int i = 0; i < data.length; i++){
-        int[] report = data[i].split(" ");
-        for (int j = 0; j < report.length; j++){
-          
-        }
+        int[] report = parseIntArrString(data[i]);
+        int[] sorted = Arrays.copyOf(report, report.length);
+        Arrays.sort(sorted);
+        boolean skibidi = false;
+        boolean sigma = false;
+          for (int k = 0; k < report.length; k++){
+              if (report[k] == sorted[k]){
+                  skibidi = true;
+              }else{
+                  skibidi = false;
+                  break;
+              }
+          }
+          if (!skibidi){
+          int index = 0;
+          for (int l = report.length -1;l >=0; l--){
+              if (report[index] == sorted[l]){
+                  skibidi = true;
+              }else{
+                  skibidi = false;
+                  break;
+              }
+              index ++;
+          }}
+          if (skibidi) {
+              for (int j = 1; j < report.length; j++) {
+                  if (Math.abs(report[j] - report[j-1]) >= 1 && Math.abs(report[j] - report[j-1]) <= 3){
+                      sigma = true;
+                  }else{
+                      sigma = false;
+                      break;
+                  }
+              }
+              if (sigma){
+                  count += 1;
+              }
+          }
       }
+      return count;
+    }
+    public static int[] parseIntArrString(String a){
+        String[] arr = a.split(" ");
+        int[] result = new int[arr.length];
+        for (int i = 0; i < arr.length; i++){
+            result[i] = Integer.parseInt(arr[i]);
+        }
+        return result;
     }
   }
