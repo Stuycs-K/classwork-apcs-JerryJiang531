17acfb5a9f7acc85fccbbb2fb8267c9b01c30305
JerryJiang531 <jerryj36@nycstudents.net>
2024-12-03 22:59:36 -0500

    attempted part 1

---
+++ advent2024/day3.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+public class day3 { // replace with correct day
+    public static void main(String[] args) {
+        String[] data = parseStringArr("inputDay3.txt"); // replace with correct day
+        System.out.println(part1(data));
+    }
+
+    public static String[] parseStringArr(String filename) {
+        try {
+            File file = new File(filename);
+            Scanner input = new Scanner(file);
+            String a = "";
+            while (input.hasNextLine()) {
+                a += input.nextLine() + "\n";
+            }
+            String[] data = a.split("\n"); // replace with correct split parameter
+            return data;
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        return null;
+    }
+    public static int part1(String[] data){
+        int sum = 0;
+        for (int i = 0; i < data.length; i++){
+            String a = data[i];
+            for (int j = 0; j < a.length() -4;j++){
+                if (a.substring(j, j+3).equals("mul")){
+                    if (a.indexOf("(") - a.indexOf(")") <= 7){
+                        a = a.substring(j);
+                        j = 0;
+                        try{
+                            String c = a.substring(a.indexOf("(") +1,a.indexOf(")"));
+                            int[] b = parseIntArrString(c);
+                            sum += b[0]*b[1];
+                        }catch (Exception e){
+                            System.out.println("notsigma");
+                        }
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+    public static int[] parseIntArrString(String a){
+        String[] arr = a.split(",");
+        int[] result = new int[arr.length];
+        for (int i = 0; i < arr.length; i++){
+            result[i] = Integer.parseInt(arr[i]);
+        }
+        return result;
+    }
+}
