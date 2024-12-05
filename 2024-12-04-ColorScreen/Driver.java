24f5df91bcb35436529f01514f875ced27a47ab8
JerryJiang531 <jerryj36@nycstudents.net>
2024-12-05 18:42:55 -0500

    added a makeBorder function

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,3 +1,29 @@
 public class Driver{
+    public static void main(String[] args){
+        makeBorder();
+    }
+    public static void makeBorder(){
 
+        System.out.println();
+
+        String border = "";
+        String filler = "";
+        for (int i = 0 ; i < 80; i++){
+            border += ".";
+            filler += " ";
+        }
+        Text.color(Text.BLUE);
+        Text.go(0,0);
+        System.out.print(border);
+
+        for (int i = 0 ; i < 30; i++){
+            Text.go(i, 0);
+            System.out.print(".");
+            Text.go(i, 80);
+            System.out.println(".");
+        }
+//        Text.go(30, 0);
+        System.out.print(border);
+        System.out.println(Text.RESET);
+    }
 }
