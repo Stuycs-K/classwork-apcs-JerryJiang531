6bf8d1240f29626bec67599bb8544f9e7964dfb2
JerryJiang531 <jerryj36@nycstudents.net>
2024-12-05 19:14:34 -0500

    made Background erase previous terminal

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,18 +1,39 @@
 public class Driver{
     public static void main(String[] args){
         makeBorder();
+        int[] rand = new int[3];
+        for (int i = 0; i < 3; i++){
+            rand[i] = (int)(Math.random() *100);
+        }
+        int inc = 20;
+        for (int j = 0; j < 3; j++){
+            if (rand[j] < 25){
+                Text.color(Text.BRIGHT, Text.RED);
+            }else if (rand[j] > 75){
+                Text.color(Text.BRIGHT, Text.RED);
+            }else{
+                Text.color(Text.WHITE);
+            }
+            Text.go(2,inc);
+            inc += 20;
+            System.out.println(rand[j]);
+        }
+        System.out.println(Text.RESET);
+        Text.go(31,0);
     }
     public static void makeBorder(){
 
         System.out.println();
 
         String border = "";
-        String filler = "";
+
         for (int i = 0 ; i < 80; i++){
             border += ".";
-            filler += " ";
+            for (int j = 0; j < 30; j++){
+                Text.erase(j,i,1);
+            }
         }
-        Text.color(Text.BLUE);
+        Text.color(Text.BLUE, Text.background(Text.YELLOW));
         Text.go(0,0);
         System.out.print(border);
 
