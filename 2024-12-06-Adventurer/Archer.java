e920e2e4a6146d99deb680336bea3dc6e3ea967d
JerryJiang531 <jerryj36@nycstudents.net>
2024-12-06 09:45:44 -0500

    added some fields and accessor methods

--- 2024-12-06-Adventurer/Archer.java
+++ 2024-12-06-Adventurer/Archer.java
@@ -1,3 +1,27 @@
 public class Archer extends Adventurer{
-  
+  private int special = 0;
+  private int specialMax = 50;
+  public String getSpecialName(){
+    return "Ammo";
+  };
+  //accessor methods
+  public int getSpecial(){
+    return special;
+  }
+  public void setSpecial(int n){
+    special = n;
+  }
+  public int getSpecialMax(){
+    return specialMax;
+  }
+  public String attack(Adventurer other);
+
+  //heall or buff the target adventurer
+  public abstract String support(Adventurer other);
+
+  //heall or buff self
+  public abstract String support();
+
+  //hurt or hinder the target adventurer, consume some special resource
+  public abstract String specialAttack(Adventurer other);
 }
