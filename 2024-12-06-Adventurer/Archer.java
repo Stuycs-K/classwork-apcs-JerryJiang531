646269db723bff50e3aa975d47a9634b295a8a46
JerryJiang531 <jerryj36@nycstudents.net>
2024-12-06 10:00:10 -0500

    wrote no arg support, made everything use getters and setters

--- 2024-12-06-Adventurer/Archer.java
+++ 2024-12-06-Adventurer/Archer.java
@@ -16,8 +16,8 @@ public class Archer extends Adventurer{
   }
   public String attack(Adventurer other){
     other.applyDamage(10);
-    special --;
-    return "Attacked " + other + " for 10 damage!\n" + getSpecial() + " reduced by 1";
+    setSpecial(getSpecial - 1);
+    return "Attacked " + other + " for 10 damage!\n" + getSpecialName() + " reduced by 1";
   }
 
   //heall or buff the target adventurer
@@ -27,12 +27,15 @@ public class Archer extends Adventurer{
   }
 
   //heall or buff self
-  public String support();
+  public String support(){
+      setSpecial(getSpecial + 3);
+      return "Reloaded!\n" + getSpecialName() + " increased by 3";
+  }
 
   //hurt or hinder the target adventurer, consume some special resource
   public String specialAttack(Adventurer other){
     other.applyDamage(100);
     setSpecial(getSpecial() - 10);
-    return "Rapid Fire at " + other + " for 100 damage!\n" + getSpecial() +  " reduced by 10";
+    return "Rapid Fire at " + other + " for 100 damage!\n" + getSpecialName() +  " reduced by 10";
   }
 }
