0a340df487c50436f1e9406750fb4c43e24f754b
JerryJiang531 <jerryj36@nycstudents.net>
2024-12-09 10:14:51 -0500

    end of classwork:

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -3,13 +3,39 @@ public class Game{
   public static void main(String[] args) {
     //do this once
     Scanner userInput = new Scanner(System.in);
-
+    Adventurer player;
+    Adventurer enemy;
     //You can do the rest many times:
-
     System.out.println("Enter username");
     //Read one line of user input
     String userName = userInput.nextLine();
     //Do something with the input
     System.out.println("Username is: " + userName);
+
+    System.out.println("Enter enemy name");
+    //Read one line of user input
+    String enemyName = userInput.nextLine();
+    //Do something with the input
+    System.out.println("Enemy name is: " + enemyName);
+
+    boolean classNotValid = true;
+    while (classNotValid){
+      System.out.println("Play as ___; Type: CodeWarrior/Archer");
+      String class = userInput.nextLine();
+      if (class.equals("CodeWarrior") || class.equals("Archer")){
+        classNotValid = false;
+      }
+    }
+    if (class.equals("CodeWarrior")){
+      player = new CodeWarrior(userName);
+      enemy = new Archer(enemyName);
+    }else{
+      player = new Archer(userName);
+      enemy = new CodeWarrior(enemyName);
+    }
+    System.out.println("Player: " + player.getName() + " "  + player.getHP() + "/ "  + player.getMaxHP() + "HP "  + player.getSpecial() + "/ "  + player.getSpecialMax() + " " + player.getSpecialName() + " ");
+    // while(){
+    //   System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+    // }
   }
 }
