53419b06570fb81e0c9bd834a5b1f8e0af403a01
JerryJiang531 <jerryj36@nycstudents.net>
2024-12-09 19:13:21 -0500

    added helper functions to reduce redundant code, made enemy Engine and attack player

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -34,10 +34,9 @@ public class Game{
       player = new Archer(userName);
       enemy = new CodeWarrior(enemyName);
     }
-    String playerInfo = "Player: " + player.getName() + " "  + player.getHP() + "/"  + player.getmaxHP() + " HP "  + player.getSpecial() + "/"  + player.getSpecialMax() + " " + player.getSpecialName() + " " + player.getSpecial() + "/" + player.getSpecialMax();
-    String enemyInfo = "Enemy: " + enemy.getName() + " "  + enemy.getHP() + "/"  + enemy.getmaxHP() + " HP "  + enemy.getSpecial() + "/"  + enemy.getSpecialMax() + " " + enemy.getSpecialName() + " " + enemy.getSpecial() + "/" + enemy.getSpecialMax();
-    System.out.println(playerInfo);
-    System.out.println(enemyInfo);
+
+    System.out.println("Player : " + getInfo(player));
+    System.out.println("Enemy : " + getInfo(enemy));
 
     boolean gameActive = true;
     while(gameActive){
@@ -50,16 +49,39 @@ public class Game{
           moveNotValid = false;
         }
       }
-      if (playerMove.equals("a")){
-        player.attack(enemy);
-      }else if (playerMove.equals("sp")){
-        player.specialAttack(enemy);
-      }else if (playerMove.equals("su")){
-        player.support();
-      }else if (playerMove.equals("quit")){
+
+      if (playerMove.equals("quit")){
         System.out.println(player.getName() + " ran away from the fight! He must be a coward...");
         break;
+      }else{
+        engine(playerMove, player, enemy);
       }
+      System.out.println("Player : " + getInfo(player));
+      System.out.println("Enemy : " + getInfo(enemy));
+      // Enemy's move
+      String enemyMove = enemyEngine();
+      engine(enemyMove, enemy, player);
+      System.out.println("Player : " + getInfo(player));
+      System.out.println("Enemy : " + getInfo(enemy));
+
     }
+  }
+  public static String enemyEngine(){
+    String[] arr = {"a", "sp", "su"};
+    int randIndex = (int) (Math.random()*3);
+    return arr[randIndex];
+  }
+  public static void engine(String move, Adventurer a, Adventurer b){
+    if (move.equals("a")){
+      System.out.println(a.attack(b));
+    }else if (move.equals("sp")){
+      System.out.println(a.specialAttack(b));
+    }else if (move.equals("su")) {
+      System.out.println(a.support());
+    }
+  }
+  public static String getInfo(Adventurer a){
+    return a + " "  + a.getHP() + "/"  + a.getmaxHP() + " HP, "  + a.getSpecial() + "/"  + a.getSpecialMax() + " " + a.getSpecialName();
+
   }
 }
