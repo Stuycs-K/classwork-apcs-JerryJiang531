2ace163e912918cf4103d58d0825fc659d1cdf8a
JerryJiang531 <jerryj36@nycstudents.net>
2025-01-02 10:14:47 -0500

    fib recursion

---
+++ 2025-01-02-fib/fib.java
@@ -0,0 +1,13 @@
+public class fib{
+  public static void main(String[] args){
+    int n = Integer.parseInt(args[0]);
+    System.out.println(fib(n));
+  }
+  public static int fib(int n){
+    if (n < 2){
+      return n;
+    }else{
+      return fib(n-1) + fib(n-2);
+    }
+  }
+}
